411ce831e177c41f6520115cbc15718a58bc4154
Angel Trinh <trinh.angel@gmail.com>
2017-09-14 14:27:25 -0400

    first commit

---
+++ hw2.py
@@ -0,0 +1,35 @@
+#Exercise 3: Numbers and Math
+#Complete   the   following   exercises
+#a. Find   the   average   of   following   numbers,   assign   it
+# to   a   variable,   print   the   average and   total   numbers
+#used   for   the   average:
+#i. For   Example: #For   numbers  44, 64, 88, 53, 89 when   you   run
+#   the   file   it should print something like: The  average  of  5
+#   given   numbers   is   : 67.6.
+#ii. 39,   45,   55,   90,   95,   96
+#iii. 54,   -45,   -10,   90
+#iv. 55,65,75,95,32
+
+print("The average of 5 given numbers is :", (44 + 64 + 88 + 53 + 89)/5)
+
+print("The average of 6 given numbers is :", (39 + 45 + 55 + 90 + 95 + 96)/6)
+
+print("The average of 4 given numbers is :", (54 - 45 -10 + 90)/4)
+
+print("The average of 5 given numbers is :", (55 + 65 + 75 + 95 + 32)/5)
+
+#b. Write   a   program   that   tells   if   the   following
+#specific   numbers   are   even   or   odd.
+#For   example
+#1. for   number   4,   your   output   should   be   something   like,
+# 4:   is   an   even number
+#2. for   number   9,   your   output   should   be   something   like,
+#9   is   an   odd number.
+#ii. 9
+#iii. 19
+#iv. 20
+
+print("4 is an even number", (4 % 2 == 0))
+print("9 is an odd number", (9 % 2 != 0))
+print("19 is an odd number", (19 % 2 !=0 ))
+print("20 is an even number", (20 % 2 == 0))
